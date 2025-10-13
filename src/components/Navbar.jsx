5a23d433d04a17b790cd517e9ab9d03aef308bf9
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-10-13 10:25:57 +0100

    Styled the Navbar to be responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ import Logo from '../assets/logo.png'
 function Navbar() {
   return (
     <header>
-        <nav className='flex flex-col items-start border-b-1 border-b-gray-400'>
-            <div className='flex items-center justify-center m-2 p-1.5'>
-                <img src={Logo} alt="skillvault-logo" className='size-12'/>
-                <h1 className='font-semibold font-sans text-2xl text-lime-950 '>SKILL VAULT</h1>
+        <nav className='flex flex-col items-start border-b-1 border-b-gray-300 md:flex-row md:items-center md:justify-evenly'>
+            <div className='flex items-center justify-start mx-0 m-2 p-1.5 md:justify-center'>
+                <img src={Logo} alt="skillvault-logo" className='size-12 mr-2'/>
+                <h1 className='font-semibold font-sans text-2xl text-lime-950'>SKILL VAULT</h1>
             </div>
-            <div className='w-full p-1.5'>
-                <ul className='w-full flex flex-1 items-center justify-between p-2 '>
+            <div className='w-full p-1.5 md:w-1/2 md:justify-self-center'>
+                <ul className='w-full flex flex-1 items-center justify-between p-2 md:flex-2 md:justify-evenly'>
                     <li><NavLink to="/" className={({ isActive }) => isActive? 'nav-link active': 'nav-link'}>Dashboard</NavLink></li>
                     <li><NavLink to="/goals">Goals</NavLink></li>
                     <li><NavLink to="/projects">Projects</NavLink></li>
