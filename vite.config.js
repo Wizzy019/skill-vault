b0b5c3ba729d13734bf41894af80326f4a915b4b
Wizzy019 <kingwizzyndukwu@gmail.com>
2025-10-23 16:24:46 +0100

    Setup up for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { loadEnv } from 'vite'
+import process from 'process'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+  const isVercel = env.VERCEL === '1'
+
+  return{
+    plugins: [react(), tailwindcss()],
+    base: isVercel ? '/' : '/skill-vault/',
+  }
 })
